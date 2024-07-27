2cfc448d0898c1d4af8f11103efff23bfa571944
Tihomir13 <tihomir13n2002@abv.bg>
2024-07-27 12:30:06 +0300

    updating form responsiveness and separating the form from the section

--- src/app/main/contact-us-tab/contact-us-tab.component.ts
+++ src/app/main/contact-us-tab/contact-us-tab.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-contact-us-tab',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './contact-us-tab.component.html',
   styleUrl: './contact-us-tab.component.scss'
 })
