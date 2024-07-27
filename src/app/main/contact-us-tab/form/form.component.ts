993bab4fa1cc4ee6594753f8552afb20fbd84f25
Tihomir13 <tihomir13n2002@abv.bg>
2024-07-27 13:51:43 +0300

    adding email functionality

--- src/app/main/contact-us-tab/form/form.component.ts
+++ src/app/main/contact-us-tab/form/form.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrls: ['./form.component.scss'],
+  imports: [ReactiveFormsModule],
 })
 export class FormComponent {
+  form = new FormGroup({
+    name: new FormControl('', { validators: [Validators.required] }),
+    tel: new FormControl('', {
+      validators: [Validators.minLength(10), Validators.required],
+    }),
+    text: new FormControl('', { validators: [Validators.required] }),
+  });
 
+  onSubmit(e: Event) {
+    e.preventDefault();
+    if (this.form.invalid) {
+      alert('Моля, попълнете всички полета.');
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        'service_foydf48',
+        'template_sqgkl9m',
+        e.target as HTMLFormElement,
+        'X1WZHJPMyAmfASPsJ'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+          alert('Съобщението беше изпратено успешно!');
+          this.form.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          alert('Възникна грешка при изпращането на съобщението.');
+        }
+      );
+  }
 }
